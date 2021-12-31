9e7c5cb03af117a54f188493c3e1e8ebcefd61ef
David Ildefonso <david_lion_ig@outlook.com.pe>
2021-12-31 07:11:28 -0500

    more test

--- cypress/integration/pokeapp.cy.js
+++ cypress/integration/pokeapp.cy.js
@@ -5,4 +5,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('user can navigate to a pokemon page', function () {
+		cy.visit('http://localhost:5000')
+		cy.contains('ivysaur').click()	
+		cy.contains('chlorophyll')
+	})  
 })
